f2572f2ffa5b13694af6ed17cc5d06e76a096adf
20twa07 <48910851+20twa07@users.noreply.github.com>
2019-12-29 22:50:17 +0100

    add fetch data & getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,28 @@
-function Home() {
-  return <>Hello</>;
+import React from 'react';
+import axios from 'axios';
+
+function Home({ products }) {
+  console.log(products);
+  // React.useEffect(() => {
+  //   getProducts();
+  // }, []);
+
+  // async function getProducts() {
+  //   const url = 'http://localhost:3000/api/products';
+  //   const response = await axios.get(url);
+  //   console.log(response.data);
+  // }
+  return <>home</>;
 }
+
+// getInitialProps allows the page to wait for data before rendering starts.
+Home.getInitialProps = async () => {
+  // fetch data in server
+  const url = 'http://localhost:3000/api/products';
+  const response = await axios.get(url);
+  return { products: response.data };
+  // return response data as an object
+  // note: this object will be merged with existing prop
+};
 
 export default Home;
