6f52e1282b4f160061a98ce37bc60390d470873a
20twa07 <48910851+20twa07@users.noreply.github.com>
2019-12-30 23:23:36 +0100

    add baseUrl

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import ProductList from '../components/Index/ProductList';
+import baseUrl from '../utils/baseUrl';
 
 function Home({ products }) {
   return <ProductList products={products} />;
@@ -9,7 +10,7 @@ function Home({ products }) {
 // getInitialProps allows the page to wait for data before rendering starts.
 Home.getInitialProps = async () => {
   // fetch data in server
-  const url = 'http://localhost:3000/api/products';
+  const url = `${baseUrl}/api/products`;
   const response = await axios.get(url);
   return { products: response.data };
   // return response data as an object
