8afe3adab057ed7bc3be16b139362ef1109421f9
20twa07 <48910851+20twa07@users.noreply.github.com>
2019-12-30 23:11:18 +0100

    delete commented code

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import axios from 'axios';
+import ProductList from '../components/Index/ProductList';
 
 function Home({ products }) {
-  console.log(products);
-  // React.useEffect(() => {
-  //   getProducts();
-  // }, []);
-
-  // async function getProducts() {
-  //   const url = 'http://localhost:3000/api/products';
-  //   const response = await axios.get(url);
-  //   console.log(response.data);
-  // }
-  return <>home</>;
+  return <ProductList products={products} />;
 }
 
 // getInitialProps allows the page to wait for data before rendering starts.
