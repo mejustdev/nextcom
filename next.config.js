9f2500f3bbf698b0d4f9fb7a3d96f5377904ef62
20twa07 <48910851+20twa07@users.noreply.github.com>
2020-01-01 12:52:05 +0100

    add jwt secret

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     MONGO_SRV:
       'mongodb+srv://mergorgec:mypassword@reactreserve-fzux2.mongodb.net/test?retryWrites=true&w=majority',
-    JWT_SECRET: '<insert-jwt-secret>',
+    JWT_SECRET: 'mysecret',
     CLOUDINARY_URL: 'https://api.cloudinary.com/v1_1/mergorgec/image/upload',
     STRIPE_SECRET_KEY: '<insert-stripe-secret-key>',
   },
