7d4880a3ec94c210db0df9ff982fe7464ad148ce
20twa07 <48910851+20twa07@users.noreply.github.com>
2020-01-02 09:23:27 +0100

    add user role functionality

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -2,14 +2,17 @@ import { Menu, Container, Image, Icon } from 'semantic-ui-react';
 import Link from 'next/link';
 import Router, { useRouter } from 'next/router';
 import NProgress from 'nprogress';
+import { handleLogout } from '../../utils/auth';
 
 Router.onRouteChangeStart = () => NProgress.start();
 Router.onRouteChangeComplete = () => NProgress.done();
 Router.onRouteChangeError = () => NProgress.done();
 
-function Header() {
+function Header({ user }) {
   const router = useRouter();
-  const user = false;
+  const isRoot = user && user.role === 'root';
+  const isAdmin = user && user.role === 'admin';
+  const isRootOrAdmin = isRoot || isAdmin;
 
   function isActive(route) {
     return route === router.pathname;
@@ -24,28 +27,33 @@ function Header() {
             ReactReserve
           </Menu.Item>
         </Link>
+
         <Link href='/cart'>
           <Menu.Item header active={isActive('/cart')}>
-            <Icon name='cart' size='large' style={{ marginRight: '1em' }} />
+            <Icon name='cart' size='large' />
             Cart
           </Menu.Item>
         </Link>
-        <Link href='/create'>
-          <Menu.Item header active={isActive('/create')}>
-            <Icon name='add square' size='large' style={{ marginRight: '1em' }} />
-            Create
-          </Menu.Item>
-        </Link>
+
+        {isRootOrAdmin && (
+          <Link href='/create'>
+            <Menu.Item header active={isActive('/create')}>
+              <Icon name='add square' size='large' />
+              Create
+            </Menu.Item>
+          </Link>
+        )}
+
         {user ? (
           <>
             <Link href='/account'>
               <Menu.Item header active={isActive('/account')}>
-                <Icon name='user' size='large' style={{ marginRight: '1em' }} />
+                <Icon name='user' size='large' />
                 Account
               </Menu.Item>
             </Link>
 
-            <Menu.Item header>
+            <Menu.Item onClick={handleLogout} header>
               <Icon name='sign out' size='large' />
               Logout
             </Menu.Item>
@@ -54,13 +62,14 @@ function Header() {
           <>
             <Link href='/login'>
               <Menu.Item header active={isActive('/login')}>
-                <Icon name='sign in' size='large' style={{ marginRight: '1em' }} />
+                <Icon name='sign in' size='large' />
                 Login
               </Menu.Item>
             </Link>
+
             <Link href='/signup'>
               <Menu.Item header active={isActive('/signup')}>
-                <Icon name='signup' size='large' style={{ marginRight: '1em' }} />
+                <Icon name='signup' size='large' />
                 Signup
               </Menu.Item>
             </Link>
