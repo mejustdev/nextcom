4531293a87a2d5415f5bad41779d30852a0e6b14
20twa07 <48910851+20twa07@users.noreply.github.com>
2020-01-03 20:22:36 +0100

    add Cascade Delete upon Document Removal

--- pages/api/product.js
+++ pages/api/product.js
@@ -1,5 +1,6 @@
 import Product from '../../models/Product';
 import connectDb from '../../utils/connectDb';
+import Cart from '../../models/Cart';
 
 connectDb();
 
@@ -28,8 +29,21 @@ async function handleGetRequest(req, res) {
 
 async function handleDeleteRequest(req, res) {
   const { _id } = req.query;
-  await Product.findOneAndDelete({ _id });
-  res.status(204).json({});
+  try {
+    // 1) Delete product by id
+    await Product.findOneAndDelete({ _id });
+    // 2) Remove product from all carts, referenced as 'product'
+    await Cart.updateMany(
+      {
+        'products.product': _id,
+      },
+      { $pull: { products: { product: _id } } },
+    );
+    res.status(204).json({});
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error deleting product');
+  }
 }
 
 async function handlePostRequest(req, res) {
