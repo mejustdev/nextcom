dfa02490c774f632cf6f48e770e76f14923f40a5
20twa07 <48910851+20twa07@users.noreply.github.com>
2020-01-03 08:13:14 +0100

    create StrıpeCheckout component

--- components/Cart/CartSummary.js
+++ components/Cart/CartSummary.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
 import { Button, Segment, Divider } from 'semantic-ui-react';
 import calculateCartTotal from '../../utils/calculateCartTotal';
 
-function CartSummary({ products }) {
+function CartSummary({ products, handleCheckout, success }) {
   const [cartAmount, setCartAmount] = React.useState(0);
   const [stripeAmount, setStripeAmount] = React.useState(0);
   const [isCartEmpty, setCartEmpty] = React.useState(false);
@@ -18,13 +19,26 @@ function CartSummary({ products }) {
       <Divider />
       <Segment clearing size='large'>
         <strong>Sub total:</strong> ${cartAmount}
-        <Button
-          icon='cart'
-          disabled={isCartEmpty}
-          color='teal'
-          floated='right'
-          content='Checkout'
-        />
+        <StripeCheckout
+          name='React Reserve'
+          amount={stripeAmount}
+          image={products.length > 0 ? products[0].product.mediaUrl : ''}
+          currency='USD'
+          shippingAddress={true}
+          billingAddress={true}
+          zipCode={true}
+          stripeKey='pk_test_5pINntvnz3SviUV4sjBvkOAP00iON8kXKZ'
+          token={handleCheckout}
+          triggerEvent='onClick'
+        >
+          <Button
+            icon='cart'
+            disabled={isCartEmpty || success}
+            color='teal'
+            floated='right'
+            content='Checkout'
+          />
+        </StripeCheckout>
       </Segment>
     </>
   );
