62d730e5e6e7b58f52af5f0be04691fd8d1acc2a
20twa07 <48910851+20twa07@users.noreply.github.com>
2020-01-03 11:33:38 +0100

    add payload for page & size

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,27 @@ import React from 'react';
 import axios from 'axios';
 import ProductList from '../components/Index/ProductList';
 import baseUrl from '../utils/baseUrl';
+import ProductPagination from '../components/Index/ProductPagination';
 
-function Home({ products }) {
-  return <ProductList products={products} />;
+function Home({ products, totalPages }) {
+  return (
+    <>
+      <ProductList products={products} />
+      <ProductPagination totalPages={totalPages} />
+    </>
+  );
 }
 
 // getInitialProps allows the page to wait for data before rendering starts.
-Home.getInitialProps = async () => {
+Home.getInitialProps = async ctx => {
+  // console.log(ctx.query);
+  const page = ctx.query.page ? ctx.query.page : '1';
+  const size = 9;
   // fetch data in server
   const url = `${baseUrl}/api/products`;
-  const response = await axios.get(url);
-  return { products: response.data };
+  const payload = { params: { page, size } };
+  const response = await axios.get(url, payload);
+  return response.data;
   // return response data as an object
   // note: this object will be merged with existing prop
 };
