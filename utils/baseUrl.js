31dd561612fbc44d239f154aa05aae8cc46efdcf
20twa07 <48910851+20twa07@users.noreply.github.com>
2020-01-09 10:56:07 +0100

    prepare for deployment

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,4 +1,6 @@
 const baseUrl =
-  process.env.NODE_ENV === 'production' ? 'https://e-reserve.now.sh' : 'http://localhost:3000';
+  process.env.NODE_ENV === 'production'
+    ? 'https://react-next-e-commerce.herokuapp.com'
+    : 'http://localhost:3000';
 
 export default baseUrl;
