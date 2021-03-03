93e8209a5d52a68cb2c5be9ba286fd6369c6916c
Me <mergorgec@gmail.com>
2021-03-03 12:08:58 +0100

    deployment vercel

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,3 @@
-const baseUrl =
-  process.env.NODE_ENV === 'production'
-    ? 'https://react-next-e-commerce.herokuapp.com'
-    : 'http://localhost:3000';
+const baseUrl = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3000';
 
 export default baseUrl;
